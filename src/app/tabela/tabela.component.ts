81251fa5569549e42c83630fd995e959b6f68bc0
Alisson Marques <xdragondev@gmail.com>
2023-05-31 06:10:31 -0300

    Criando forma de filtragem dinamica de uma lista de site

--- src/app/tabela/tabela.component.ts
+++ src/app/tabela/tabela.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { WebSitesDto } from '../Dto/WebSitesDto';
 
 @Component({
@@ -7,23 +7,11 @@ import { WebSitesDto } from '../Dto/WebSitesDto';
   styleUrls: ['./tabela.component.css']
 })
 export class TabelaComponent {
-  public webSites: WebSitesDto[] = [];
+  @Input() webSites: WebSitesDto[] = [];
   public siteBuscados: WebSitesDto[] = [];
 
   ngOnInit(){
-    this.GerarListaSites();
     this.FiltrarListaSitesDisponiveis();
-  }
-
-  public GerarListaSites()
-  {
-    this.webSites = [
-      {nome:'Angular.io', url:'https://angular.io', descricao:'Documentação oficial do Angular, com guias, tutoriais e exemplos'},
-      {nome:'Angular Blog', url:'https://blog.angular.io', descricao:'Blog oficial do Angular, com atualizações, anúncios e artigos'},
-      {nome:'Scotch.io', url:'https://scotch.io/tag', descricao:'Artigos, tutoriais e dicas sobre desenvolvimento Angular'},
-      {nome:'AngularInDepth', url:'https://indepth.dev/tag/angular', descricao:'Artigos técnicos aprofundados sobre Angular'},
-      {nome:'Angular University', url:'https://blog.angular-university.io', descricao:'Recursos educacionais, tutoriais e cursos sobre Angular'},
-    ]
   }
 
   public FiltrarListaSitesDisponiveis()
